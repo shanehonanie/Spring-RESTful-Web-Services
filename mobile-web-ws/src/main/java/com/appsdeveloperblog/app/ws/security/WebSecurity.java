00f3c229031272507a29ce76094a088109e5891f
Shane Honanie <shaneh425@gmail.com>
2019-11-03 11:39:50 -0800

    Changed Login URL

--- mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -25,7 +25,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
         http.csrf().disable().authorizeRequests()
         .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
         .permitAll()
-        .anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+        .anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
         
     }
 
@@ -33,5 +33,12 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
+    
+    public AuthenticationFilter getAuthenticationFilter() throws Exception {
+	    final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+	    filter.setFilterProcessesUrl("/users/login");
+	    return filter;
+	}
 	
+    
 }
