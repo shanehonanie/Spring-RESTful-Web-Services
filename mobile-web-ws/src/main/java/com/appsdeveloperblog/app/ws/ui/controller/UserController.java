51576356e01da21f6f5efcca4b7403ce0e3e898f
Shane Honanie <shaneh425@gmail.com>
2019-11-04 13:26:05 -0800

    Get All Users with Pagination API Call

--- mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appsdeveloperblog.app.ws.exceptions.UserServiceException;
@@ -28,6 +32,25 @@ public class UserController {
 	
 	@Autowired
 	UserService userService;
+	
+	@GetMapping(
+			produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
+			)
+	public List<UserRest> getAllUsers(@RequestParam(value="page", defaultValue = "0") int page,
+			@RequestParam(value="limit", defaultValue = "25") int limit) {
+		List<UserRest> returnValue = new ArrayList<>();
+		
+		List<UserDto> users = userService.getAllUsers(page, limit);
+		
+			for (UserDto userDto : users) {
+				UserRest userModel = new UserRest();
+				BeanUtils.copyProperties(userDto, userModel);
+				returnValue.add(userModel);
+			}
+	
+		
+		return returnValue;
+	}
 	
 	@GetMapping(path="/{id}",
 			produces= {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
