fe2ec0d28ff68d5e4f55af36e323b9867056ea82
Shane Honanie <shaneh425@gmail.com>
2019-11-05 21:53:26 -0800

    Address Entity & Create User with Address

--- mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/shared/dto/UserDto.java
+++ mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/shared/dto/UserDto.java
@@ -1,6 +1,7 @@
 package com.appsdeveloperblog.app.ws.shared.dto;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class UserDto implements Serializable {
 	private static final long serialVersionUID = 6835192601898364280L;
@@ -13,6 +14,7 @@ public class UserDto implements Serializable {
 	private String encryptedPassword;
 	private String emailVerificationToken;
 	private Boolean emailVerificationStatus = false;
+	private List<AddressDTO> addresses;
 
 	public long getId() {
 		return id;
@@ -84,6 +86,14 @@ public class UserDto implements Serializable {
 
 	public void setEmailVerificationStatus(Boolean emailVerificationStatus) {
 		this.emailVerificationStatus = emailVerificationStatus;
+	}
+
+	public List<AddressDTO> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<AddressDTO> addresses) {
+		this.addresses = addresses;
 	}
 
 }
--- mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-web-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -74,11 +75,13 @@ public class UserController {
 		if(userDetails.getFirstName().isEmpty() || userDetails.getLastName().isEmpty() ||
 				userDetails.getEmail().isEmpty() || userDetails.getPassword().isEmpty()) throw new UserServiceException(ErrorMessages.MISSING_REQUIRED_FIELD.getErrorMessage());
 
-		UserDto userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails, userDto);
+//		UserDto userDto = new UserDto();
+//		BeanUtils.copyProperties(userDetails, userDto);
+		ModelMapper modelMapper = new ModelMapper();
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
 		
 		UserDto createdUser = userService.createUser(userDto);
-		BeanUtils.copyProperties(createdUser, returnValue);
+		returnValue = modelMapper.map(createdUser, UserRest.class);
 
 		return returnValue;
 	}
