6280d127f8c72a8230c9cedf0e3716a803e16787
Shane Honanie <shaneh425@gmail.com>
2019-11-11 11:55:01 -0800

    H2 DB Support

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/security/WebSecurity.java
@@ -31,12 +31,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
         .permitAll()
         .antMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_URL)
         .permitAll()
+        .antMatchers(SecurityConstants.H2_CONSOLE)
+        .permitAll()
         .anyRequest().authenticated().and()
         .addFilter(getAuthenticationFilter())
         .addFilter(new AuthorizationFilter(authenticationManager()))
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         
+        http.headers().frameOptions().disable();
     }
 
     @Override
